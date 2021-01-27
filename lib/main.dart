2463090f043a8fa004e701e64ad0f3899e54a5e1
11mrhappy <nekomamiremy@gmail.com>
2021-01-27 23:51:20 +0900

    basicly

--- lib/main.dart
+++ lib/main.dart
@@ -1,26 +1,42 @@
-import 'dart:convert';
 import 'dart:async';
-
+import 'dart:convert';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
-
 void main() {
-  runApp(MyApp());
+  runApp(new MaterialApp(
+    home: new HomePage(),
+  ));
 }
 
-Future<http.Response> fetchAlbum() {
-  return http.get('https://connpass.com/api/v1/event/event_id=200543');
+class HomePage extends StatefulWidget {
+  @override
+  HomePageState createState() => new HomePageState();
 }
 
-class MyApp extends StatelessWidget {
+class HomePageState extends State<HomePage> {
+  List data;
+  Future<String> getData() async {
+    var response = await http.get(
+        Uri.encodeFull('https://connpass.com/api/v1/event/event_id=200543'),
+        headers: {
+          "Accept": "application/json"
+        }
+    );
+    data = json.decode(response.body);
+    print(data[1]["title"]);
+
+    return "Success!";
+  }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Connpass API App',
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+    return new Scaffold(
+      body: new Center(
+        child: new RaisedButton(
+          child: new Text("Get data"),
+          onPressed: getData,
+        ),
       ),
     );
   }
